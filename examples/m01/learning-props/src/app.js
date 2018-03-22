d4fe5e8ff1b00418c004dcdc7e1cd799c1d914a8
razevedo <razevedo@TF-1933>
2018-03-22 09:57:50 -0300

    Component Lifecycle

--- examples/m01/learning-props/src/app.js
+++ examples/m01/learning-props/src/app.js
@@ -7,12 +7,35 @@ import LikeButton from './like-button'
 import SearchButton from './search-button'
 import StateExample1 from './state-example1'
 import ColorButton from './color-button'
-
+import Timer from './timer'
+import Button from './button';
 class App extends Component {
+  constructor() {
+    console.log('constructor')
+    super()
+    this.state = {
+      time: 0,
+      showTimer: true
+    }
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount app')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount app')
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount app')
+  }
+
   render() {
+    console.log('render app')
     return (
       <div className='container' >
-        <Title name="João" />
+        <Title />
 
         {['blue', 'red', 'green'].map((square, index) => (
           <Square key={index} color={square} />
@@ -22,6 +45,20 @@ class App extends Component {
         <SearchButton />
         <StateExample1 />
         <ColorButton />
+
+        {this.state.showTimer && <Timer time={this.state.time} />}
+
+        <button onClick={() => {
+          this.setState({ showTimer: !this.state.showTimer })
+        }}>Show / Hide Timer
+        </button>
+
+        <Timer time={this.state.time} />
+
+        <button onClick={() => {
+          this.setState({ time: this.state.time + 10 })
+        }}>Change props
+        </button>
       </div>
     )
   }
