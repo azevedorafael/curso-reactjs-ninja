5055d216f30c873a859f92beb8f28db1a26c9b1f
Rafael da S.P. de Azevedo <rafaelsilva.fatec@gmail.com>
2018-03-22 22:33:38 -0300

    Controlled input form

--- examples/m01/learning-forms/src/app.js
+++ examples/m01/learning-forms/src/app.js
@@ -3,33 +3,23 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super()
     this.state = {
-      checked: false,
-      showContent: false
+      value: 'Valor inicial'
     }
   }
-  render () {
+  render() {
     return (
       <div>
-        <label>
-          <input
-            type='checkbox'
-            checked={this.state.checked}
-            onChange={() => {
-              this.setState({
-                checked: !this.state.checked
-              }, () => {
-                this.setState({
-                  showContent: this.state.checked
-                })
-              })
-            }}
-          /> Mostrar conteúdo
-        </label>
-
-        {this.state.showContent && <div>Olha eu aqui!</div>}
+        <form>
+          <input type="text" value={this.state.value} onChange={(e) => {
+            console.log(e)
+            this.setState({
+              value: e.target.value
+            })
+          }} />
+        </form >
       </div>
     )
   }
